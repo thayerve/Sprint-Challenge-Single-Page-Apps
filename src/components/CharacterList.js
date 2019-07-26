c3732d80e88e4cd3bdbf7f10e65cea025de2ade6
Virginia Thayer <virginia@stripe.com>
2019-07-26 08:38:11 -0700

    Set up project.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import CharacterCard from './CharacterCard';
+import { Grid } from "semantic-ui-react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
